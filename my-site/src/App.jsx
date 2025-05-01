b8d8b61e65439dfabdc3ed5349c7dbfd737287a5
k10-k10 <keito.tottori@gmail.com>
2025-05-01 22:25:16 +0900

    Add my-site files

---
+++ my-site/src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, React from GitHub Pages!</h1>
+    </div>
+  );
+}
+
+function Top_Button() {
+  return (
+    <div>
+      <button type="button" size="large" onClick={() => alert('test')}>test</button>
+    </div>
+  )
+}
+function Back_Button() {
+  return (
+    <div>
+      <button type="button" size="large" onClick={() => alert('test')}>test</button>
+    </div>
+  );
+}
+
+
+
+export default App;
+export { Top_Button, Back_Button, Tests };
