255a0e3100b01044ae5530fe47dbcdad0898d923
K10-K10 <keito.tottori@gmail.com>
2025-08-31 22:50:51 +0900

    Update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello react
+    </div>
+  );
+}
+
+function Test() {
+  return (
+    <div className='Test'>
+      React Test
+    </div>
+  );
+}
+
+
+export default App;
