0cd997cec5dd7ddd83b7d2a7d56cd6ecfadfee51
K10-K10 <keito.tottori@gmail.com>
2025-09-01 23:57:00 +0900

    Update image

--- src/footer.js
+++ src/footer.js
@@ -3,10 +3,14 @@ import './index.css'
 function Footer() {
 	return (
 		<footer>
-			<div className="footer_icon"><img src="/react_logo.png" alt="React icon" /></div>
-			<div className="footer_icon"><img src="/github_logo.png" alt="Github icon" /></div>
-			<div className="footer_info">2025</div>
-			<div className="footer_info">React & Github Pages</div>
+			<div className="footer_icon">
+				<img src="/react_logo.png" alt="React icon" />
+				<img src="/github_logo.png" alt="Github icon" />
+				</div>
+			<div className="footer_info">
+				<a>2025</a>
+				<a>React & Github Pages</a>
+			</div>
 		</footer>
 	);
 }
