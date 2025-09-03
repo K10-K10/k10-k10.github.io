b239fabff4f52108c6d50a2ef82833c9962b12ba
K10-K10 <keito.tottori@gmail.com>
2025-09-03 17:06:10 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Se from'./pages/se/se'
 import Main from './main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<Main />
+	<div>
+		{/*<Se/>*/}
+		<Main />
+	</div>
+	
 );
---
+++ src/pages/se/se.js
@@ -0,0 +1,56 @@
+import './../RGB/RGB.css'
+import './../../index.css'
+
+function Se (){
+	return(
+		<div>
+		<fieldset>
+			<legend>Sound-1</legend>
+		<div>
+			<input type="radio" id="dewey" name="drone" value="dewey" /><p>start</p>
+			<label for="dewey">Dewey</label>
+			<input type="radio" id="louie" name="drone" value="louie" />
+			<label for="louie">Louie</label>
+		</div>
+		</fieldset>
+		<fieldset>
+			<legend>Sound-2</legend>
+		<div>
+			<input type="radio" id="dewey" name="drone_1" value="dewey" /><p>start</p>
+			<label for="dewey">Dewey</label>
+			<input type="radio" id="louie" name="drone_1" value="louie" />
+			<label for="louie">Louie</label>
+		</div>
+		</fieldset>
+		<fieldset>
+			<legend>Sound-3</legend>
+		<div>
+			<input type="radio" id="dewey" name="drone" value="dewey" /><p>start</p>
+			<label for="dewey">Dewey</label>
+			<input type="radio" id="louie" name="drone" value="louie" />
+			<label for="louie">Louie</label>
+		</div>
+		</fieldset>
+		<fieldset>
+			<legend>Sound-4</legend>
+		<div>
+			<input type="radio" id="dewey" name="drone" value="dewey" /><p>start</p>
+			<label for="dewey">Dewey</label>
+			<input type="radio" id="louie" name="drone" value="louie" />
+			<label for="louie">Louie</label>
+		</div>
+		</fieldset>
+		<fieldset>
+			<legend>Sound-5</legend>
+		<div>
+			<input type="radio" id="dewey" name="drone" value="dewey" /><p>start</p>
+			<label for="dewey">Dewey</label>
+			<input type="radio" id="louie" name="drone" value="louie" />
+			<label for="louie">Louie</label>
+		</div>
+		</fieldset>
+		</div>
+	);
+}
+
+export default Se
