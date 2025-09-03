eee1cfa99e31bd147f5a78098cf77e241e03477a
K10-K10 <keito.tottori@gmail.com>
2025-09-03 15:29:40 +0900

    Edit Title,Add link for repository and homepage

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -3,8 +3,8 @@ function Footer() {
 	return (
 		<footer>
 			<div className="footer_icon">
-				<img src="/react_logo.png" alt="React icon" />
-				<img src="/github_logo.png" alt="Github icon" />
+				<a href="https://react.dev/"><img src="/react_logo.png" alt="React icon" /></a>
+				<a href="https:github.com/K10-K10/K10-K10.github.io"><img src="/github_logo.png" alt="Github icon"/></a>
 			</div>
 			<div className="footer_info">
 				<p>2025</p>
