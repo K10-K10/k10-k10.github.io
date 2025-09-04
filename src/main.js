b16156718d31c6fdde381b809340c396fe7fb899
K10-K10 <keito.tottori@gmail.com>
2025-09-04 17:46:34 +0900

    Update directory configuration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './layouts/header';
 import Footer from './layouts/footer';
-import Home from './pages/home';
-import NotFound from './pages/404';
-import RGB from './pages/RGB';
-import SE from './pages/SE';
+import Home from './pages/home/home';
+import NotFound from './pages/404/404';
+import RGB from './pages/RGB/RGB';
+import SE from './pages/SE/SE';
 
 function main() {
 	return (
