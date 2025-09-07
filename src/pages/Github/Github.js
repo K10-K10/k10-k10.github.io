9157cb2d6e7f371e376bea788438ef6afaa5d67e
K10-K10 <keito.tottori@gmail.com>
2025-09-07 23:40:43 +0900

    Add input

--- src/pages/Github/Github.js
+++ src/pages/Github/Github.js
@@ -1,12 +1,52 @@
-import "./Github.css";
+import { useState } from "react";
+import './Github.css';
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 function Github() {
+	const [name, setName] = useState("");
+	const [repos, setRepos] = useState([]);
+
+	const handleClick = async () => {
+		if (!name) {
+			alert("Please input user name");
+			return;
+		}
+
+		try {
+			const response = await fetch(`https://api.github.com/users/${name}/repos`);
+			if (!response.ok) {
+				throw new Error("User is not found");
+			}
+			const data = await response.json();
+			setRepos(data);
+		} catch (error) {
+			console.error("Request error:", error);
+			alert("API failed");
+		}
+	};
+
 	return (
 		<div>
-			<label for="username">Username:</label>
-    	<input type="text" id="username" name="username" placeholder="Enter your username" />
+			<Input
+				type="text"
+				label="Github User Name"
+				value={name}
+				onChange={setName}
+				tmp="Input"
+			/>
+			<Button onClick={handleClick}>API</Button>
+			<ul>
+				{repos.map((repo) => (
+					<li key={repo.id}>
+						<a href={repo.html_url} target="_blank" rel="noreferrer">
+							{repo.name}
+						</a>
+					</li>
+				))}
+			</ul>
 		</div>
-	)
+	);
 }
 
 export default Github;
