0daa45349b43910efce430ab7140dba574a742f4
K10-K10 <keito.tottori@gmail.com>
2025-09-07 22:16:33 +0900

    Use Audio component

--- src/pages/SE/SE.js
+++ src/pages/SE/SE.js
@@ -1,4 +1,5 @@
 import "./SE.css";
+import Audio from "./../../components/Audio/Audio"
 
 function SE() {
 	return (
@@ -6,19 +7,23 @@ function SE() {
 			<div>
 				<fieldset>
 					<legend>BGM</legend>
-					<audio controls src="/sounds/sound1.mp3"></audio> <p>開演前後用</p>
+					<Audio src={"/sounds/sound1.mp3"}/>
+					<p>開演前後用</p>
 				</fieldset>
 				<fieldset>
 					<legend>滑ったとき</legend>
-					<audio controls src="/sounds/sound2.mp3"></audio> <p>?</p>
+					<Audio src={"/sounds/sound1.mp3"}/>
+					<p>?</p>
 				</fieldset>
 				<fieldset>
 					<legend>深刻な場面用</legend>
-					<audio controls src="/sounds/sound3.mp3"></audio> <p>?</p>
+					<Audio src={"/sounds/sound1.mp3"}/>
+					<p>?</p>
 				</fieldset>
 				<fieldset>
 					<legend>コナンBGM</legend>
-					<audio controls src="/sounds/sound4.mp3"></audio> <p>"ラピス"わ、なんか急に暗くなった</p>
+					<Audio src={"/sounds/sound1.mp3"}/>
+					<p>"ラピス"わ、なんか急に暗くなった</p>
 				</fieldset>
 			</div>
 			<div>
