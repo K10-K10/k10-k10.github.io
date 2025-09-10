366cf2b9f877c5e1ac3b1d6422390e8f61f67ba0
K10-K10 <keito.tottori@gmail.com>
2025-09-11 00:27:55 +0900

    Add spacer

---
+++ src/ui-parts/Spacer.js
@@ -0,0 +1,13 @@
+function Spacer ({style = {},}) {
+	const styles = {
+		spacer: {
+		padding: "15px",
+		...style
+		},
+	};
+	return (
+	<div style={(styles.spacer)}/>
+	);
+}
+
+export default Spacer;
