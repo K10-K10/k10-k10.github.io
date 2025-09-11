29b7d10826b6205868bc4a8a987317c6a4caeddb
K10-K10 <keito.tottori@gmail.com>
2025-09-12 01:31:36 +0900

    Update Header animation

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -4,10 +4,10 @@ function Header() {
 		<header className="header">
 				<a href="/#/" className="title">K10-K10</a>
 				<div className="topic_scope">
-					<a href="/#/Projects" className="topic">Projects</a>
-				<a href="/#/Tools" className="topic">Tools</a>
-				<a href="/#/Blog" className="topic">Blog</a>
-					<a href="/#/About" className="topic">About</a>
+					<a href="/#/Projects" className="topic_1">Projects</a>
+					<a href="/#/Tools" className="topic_2">Tools</a>
+					<a href="/#/Blog" className="topic_3">Blog</a>
+					<a href="/#/About" className="topic_4">About</a>
 			</div>
 		</header>
 	);
