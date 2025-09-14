288afe5656adff7b649a35409e0a7bde50fb8a07
K10-K10 <keito.tottori@gmail.com>
2025-09-14 17:11:05 +0900

    Add head component

--- src/layouts/Head/Head.js
+++ src/layouts/Head/Head.js
@@ -1,7 +1,7 @@
 import "./Head.css"
 import { useEffect } from "react"
 
-function Head({ title }) {
+function Head({ title , link_title}) {
 	useEffect(() => {
 		if (title) {
 			document.title = `K10-K10 | ${title}`;
@@ -9,6 +9,11 @@ function Head({ title }) {
 			document.title = "K10-K10";
 		}
 	}, [title]);
+	if (link_title) {
+			document.title = `K10-K10 | ${title}`;
+		} else {
+			document.title = "K10-K10";
+		}
 	return (
 		<div classname="head-lay">
 			<head>
@@ -16,6 +21,7 @@ function Head({ title }) {
 				<link rel="icon" href="/my_icon.png" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>{document.title}</title>
+				<meta property="og:title" content = {document.link_title}></meta>
 		</head>
 		</div>
 	)
