bd15370f8db0fc1c6b33bd6282fd172371b529b8
K10-K10 <keito.tottori@gmail.com>
2025-09-19 12:11:32 +0900

    fix head title
    Fix path

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,12 +1,14 @@
 import { posts } from "./posts";
 import { Link } from "react-router-dom";
 import Talk from "./../../layouts/Talk/Talk";
+import Head from "./../../layouts/Head/Head"
 
 function Blog() {
 	const sortedPosts = [...posts].sort((a, b) => b.id - a.id);
 
 	return (
 		<div className="blog-home">
+			<Head title="Blog"/>
 			<Talk title="Blog">
 				{sortedPosts.map(post => (
 					<div key={post.id}>
