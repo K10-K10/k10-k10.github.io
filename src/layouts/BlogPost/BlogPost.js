5ba68a50928c54aec2c1d2601c58f8727319d435
K10-K10 <keito.tottori@gmail.com>
2025-10-05 00:17:25 +0900

    Add:Button for Blog list page

--- src/layouts/BlogPost/BlogPost.js
+++ src/layouts/BlogPost/BlogPost.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { posts } from '@pages/Blog/posts';
 import Talk from '@layouts/Talk/Talk';
+import Button from '@parts/Button';
 
 function BlogPost() {
 	const { id } = useParams();
@@ -13,6 +14,9 @@ function BlogPost() {
 			<Talk title={post.title}>
 				<small style={{ textAlign: 'center', display: 'block', marginBottom: '30px' }} className='data'>{post.date}</small>
 				<pre style={{ lineHeight: '150%' }}>{post.content}</pre>
+			<a href='#/Blog' className="list-button" style={{position: 'absolute',bottom: '20%'}}>
+			<Button>Back to Blog List</Button>
+			</a>
 			</Talk>
 		</div>
 	);
