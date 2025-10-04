52c4cfb2e2d53217079e64f78ffd1b41b9447eaf
K10-K10 <keito.tottori@gmail.com>
2025-10-05 00:11:46 +0900

    Add:New ppost
    Title: Added langton's ant

--- src/pages/Blog/posts.js
+++ src/pages/Blog/posts.js
@@ -9,4 +9,18 @@ export const posts = [
 		\nI will update this blog as much as possible.\
 		',
 	},
+	{
+		id: 2,
+		title: 'Added langton\'s ant',
+		date: '04-Oct-2025',
+		content: 'I added a new page about Langton\'s Ant.\
+		\nIt was my first time creating a program using JavaScript,\
+		\nand I learned a lot from it.\
+		\nIt was difficult to make it display quickly.\
+		\n\
+		\nI want to be able to change the speed and display size.\
+		\nI might also add the “Game of Life.”\
+		\nBut, I don\'t know how to change colour in touched grid;;\
+		\nI should have to learn more...'
+	}
 ];
