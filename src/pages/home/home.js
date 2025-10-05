ee154a31cb2097f428e5ee6e76af96a08c977e2d
K10-K10 <keito.tottori@gmail.com>
2025-10-05 22:45:26 +0900

    Update:Home page layout

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,25 @@
 import './home.css';
+import { posts } from '@pages/Blog/posts';
 
 function home() {
 	return (
 		<div id='home'>
+			<h1>Hello,World!</h1>
+			<div className='information'>
+				<h2>Projects</h2>
+				<h2>Tools</h2>
+				<p>
+					New `langton's ant`
+				</p>
+			<h2>Latest Blog Posts</h2>
+			<ul>
+				{posts.slice(-3).reverse().map(p => (
+					<li key={p.headName}>
+						<a href={`/#/Blog/${p.headName}`}>{p.title}</a>
+					</li>
+				))}
+			</ul>
+			</div>
 		</div>
 	)
 }
