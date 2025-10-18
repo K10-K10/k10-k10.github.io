a1c389ab4d0eb23ad990e9d152585cca121a7eea
K10-K10 <keito.tottori@gmail.com>
2025-10-18 18:55:33 +0900

    Update:meta

--- src/layouts/Head/Head.jsx
+++ src/layouts/Head/Head.jsx
@@ -1,18 +1,18 @@
 import './Head.css';
 import { useEffect } from 'react';
 
-function Head({ title, link_title }) {
+function Head({ linkTitle, link_title, description }) {
 	useEffect(() => {
-		if (title) {
-			title = `K10-K10 | ${title}`;
+		if (linkTitle) {
+			linkTitle = `K10-K10 | ${linkTitle}`;
 		} else {
-			title = 'K10-K10';
+			linkTitle = 'K10-K10';
 		}
-	}, [title]);
+	}, [linkTitle]);
 	if (link_title) {
-		title = `K10-K10 | ${title}`;
+		linkTitle = `K10-K10 | ${linkTitle}`;
 	} else {
-		title = 'K10-K10';
+		linkTitle = 'K10-K10';
 	}
 	return (
 		<head>
@@ -20,8 +20,17 @@ function Head({ title, link_title }) {
 			<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 			<meta name='description' content={link_title} />
 			<link rel='icon' href='https://K10-K10.github.io/favicon.ico' />
-			<title>{title}</title>
+			<link rel="apple-touch-icon" href="/favicon.ico" />
+			<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+			<meta property="og:type" content="website" />
+			<meta name="twitter:domain" content="K10-K10.github.io" />
 			<meta property='og:title' content={link_title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:site_name" content={linkTitle} />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:linkTitle" content={linkTitle} />
+			<meta name="twitter:description" content={description} />
+			<meta name="theme-color" content="#00323C" />
 		</head>
 	)
 }
