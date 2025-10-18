0b13c3801cd7372dd65104568f0b37eb16e8b188
K10-K10 <keito.tottori@gmail.com>
2025-10-18 17:51:17 +0900

    Add: Vite icon

--- src/layouts/Footer/footer.jsx
+++ src/layouts/Footer/footer.jsx
@@ -4,11 +4,12 @@ function Footer() {
 		<footer>
 			<div className='footer_icon'>
 				<a href='https://react.dev/'><img src='/react_logo.png' alt='React icon' /></a>
+				<a href='https://vite.dev'><img src='/Vite.js.svg' alt='Vite icon' /></a>
 				<a href='https://github.com/K10-K10/K10-K10.github.io'><img src='/github_logo.png' alt='Github icon' /></a>
 			</div>
 			<div className='footer_info'>
 				<p>2025</p>
-				<p>React & Github Pages</p>
+				<p>React + Vite & Github Pages</p>
 			</div>
 		</footer>
 	);
