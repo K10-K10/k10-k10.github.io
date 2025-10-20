45144735e15204770c78ad9e1442ea9936ac5b1d
K10-K10 <keito.tottori@gmail.com>
2025-10-20 21:10:54 +0900

    Add:blog move button

--- src/layouts/BlogPost/BlogPost.jsx
+++ src/layouts/BlogPost/BlogPost.jsx
@@ -1,27 +1,61 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { posts } from '@pages/Blog/posts';
 import Talk from '@layouts/Talk/Talk';
 import Button from '@parts/Button';
 import NotFound from '@pages/404/404';
 
-function BlogPost() {
+export default function BlogPost() {
 	const { headName } = useParams();
+	const navigate = useNavigate();
 	const post = posts.find((p) => p.headName === headName);
 
+	if (!post) return <NotFound />;
 
-	if (!post) return (<NotFound />);
+	const handleNext = () => {
+		const nextPost = posts.find(item => item.id === post.id + 1);
+		if (nextPost) {
+			navigate(`/Blog/${nextPost.headName}`);
+		} else {
+			alert("This is the last post.");
+		}
+	};
+	const handleForward = () => {
+		const nextPost = posts.find(item => item.id === post.id - 1);
+		if (nextPost) {
+			navigate(`/Blog/${nextPost.headName}`);
+		} else {
+			alert("This is the first post.");
+		}
+	};
 
 	return (
 		<div className='blog-post'>
 			<Talk title={post.title}>
-				<small style={{ textAlign: 'center', display: 'block', marginBottom: '30px' }} className='data'>{post.date}</small>
-				<pre style={{ lineHeight: '150%', whiteSpace: 'pre-wrap' }}>{post.content}</pre>
+				<small
+					style={{ textAlign: 'center', display: 'block', marginBottom: '30px' }}
+					className='data'
+				>
+					{post.date}
+				</small>
+
+				<pre style={{ lineHeight: '150%', whiteSpace: 'pre-wrap' }}>
+					{post.content}
+				</pre>
+
+				<Button onClick={handleForward}>
+					Forward
+				</Button>
 				<a href='#/Blog' className='list-button'>
-					<Button style={{ margin: '40px 0px 20px' }}>Back to Blog List</Button>
+					<Button style={{ margin: '40px 0px 20px' }}>
+						Back to Blog List
+					</Button>
 				</a>
+
+				<Button onClick={handleNext}>
+					Next
+				</Button>
 			</Talk>
-		</div>
+		</div >
 	);
 }
 
-export default BlogPost;
