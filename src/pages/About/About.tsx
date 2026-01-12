dd08fef616ce853943a6b47c1208a3fac4321dd6
K10-K10 <keito.tottori@gmail.com>
2026-01-12 23:20:00 +0900

    formta

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,55 +4,55 @@ import Talk from "@layouts/Talk/Talk";
 import Head from "@layouts/Head/Head";
 
 export default function About() {
-  return (
-    <div className="about">
-      <Head
-        title={"About Site&Author"}
-        linkTitle={"About"}
-        description={"K10-K10 site - About page"}
-      />
-      <Talk
-        title="About the site"
-        style_scope={{ background: "var(--cloudy_background-color)" }}
-      >
-        <p>This site is my React practice site.</p>
-        <p>Content related to blogs, tools, games, and more will be added.</p>
-        <Spacer />
-        <p>This site is powered by GitHub Pages and React.</p>
-        <a href="https://github.com/K10-K10/k10-k10.github.io">
-          This site's source code
-        </a>
-      </Talk>
-      <Talk title="About the Author">
-        <div className="user-wrapper">
-          <img src="/favicon.ico" alt="author icon" />
-          <div className="user-info">
-            <p>Hello! I'm K10-K10. I'm junior high school student</p>
-            <a href="https://github.com/K10-K10">My GitHub Account</a>
-            <p>I primarily use C++, and belong to a robotics team.</p>
-            <a href="https://techno-robocup.github.io/">Robot team's blogs</a>
-          </div>
-        </div>
+	return (
+		<div className="about">
+			<Head
+				title={"About Site&Author"}
+				linkTitle={"About"}
+				description={"K10-K10 site - About page"}
+			/>
+			<Talk
+				title="About the site"
+				style_scope={{ background: "var(--cloudy_background-color)" }}
+			>
+				<p>This site is my React practice site.</p>
+				<p>Content related to blogs, tools, games, and more will be added.</p>
+				<Spacer />
+				<p>This site is powered by GitHub Pages and React.</p>
+				<a href="https://github.com/K10-K10/k10-k10.github.io">
+					This site's source code
+				</a>
+			</Talk>
+			<Talk title="About the Author">
+				<div className="user-wrapper">
+					<img src="/favicon.ico" alt="author icon" />
+					<div className="user-info">
+						<p>Hello! I'm K10-K10. I'm junior high school student</p>
+						<a href="https://github.com/K10-K10">My GitHub Account</a>
+						<p>I primarily use C++, and belong to a robotics team.</p>
+						<a href="https://techno-robocup.github.io/">Robot team's blogs</a>
+					</div>
+				</div>
 
-        <p>
-          I started programming in first grade and attended robotics classes.
-        </p>
-        <p>
-          At the same time, my father recommended Unity, and I started learning
-          C#
-        </p>
-        <p>
-          I entered junior high school and started competitive programming and
-          C++.
-        </p>
-        <p>
-          Recently, I haven't been able to join in competitions. Therefore, I
-          want to join as much as possible.
-        </p>
-        <Spacer />
-        <p>I want to update this site as much as possible.</p>
-        <p>Thank you for Reading!</p>
-      </Talk>
-    </div>
-  );
+				<p>
+					I started programming in first grade and attended robotics classes.
+				</p>
+				<p>
+					At the same time, my father recommended Unity, and I started learning
+					C#
+				</p>
+				<p>
+					I entered junior high school and started competitive programming and
+					C++.
+				</p>
+				<p>
+					Recently, I haven't been able to join in competitions. Therefore, I
+					want to join as much as possible.
+				</p>
+				<Spacer />
+				<p>I want to update this site as much as possible.</p>
+				<p>Thank you for Reading!</p>
+			</Talk>
+		</div>
+	);
 }
--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -4,27 +4,27 @@ import Talk from "@layouts/Talk/Talk";
 import Head from "@layouts/Head/Head";
 
 export default function Blog() {
-  const sortedPosts = [...posts].sort((a, b) => b.id - a.id);
+	const sortedPosts = [...posts].sort((a, b) => b.id - a.id);
 
-  return (
-    <div className="blog-home">
-      <Head
-        title={"Blog Top Page"}
-        linkTitle={"Blog"}
-        description={"K10-K10 site - Blog top page"}
-      />
-      <Talk title="Blog">
-        {sortedPosts.map((post) => (
-          <div key={post.id}>
-            <p className="blog-list">
-              {post.date} :{" "}
-              <Link className="blog-link" to={`/blog/${post.headName}`}>
-                {post.title}
-              </Link>
-            </p>
-          </div>
-        ))}
-      </Talk>
-    </div>
-  );
+	return (
+		<div className="blog-home">
+			<Head
+				title={"Blog Top Page"}
+				linkTitle={"Blog"}
+				description={"K10-K10 site - Blog top page"}
+			/>
+			<Talk title="Blog">
+				{sortedPosts.map((post) => (
+					<div key={post.id}>
+						<p className="blog-list">
+							{post.date} :{" "}
+							<Link className="blog-link" to={`/blog/${post.headName}`}>
+								{post.title}
+							</Link>
+						</p>
+					</div>
+				))}
+			</Talk>
+		</div>
+	);
 }
