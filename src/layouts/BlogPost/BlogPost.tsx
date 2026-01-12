59ef296cd22d69ab1c2a628a31faff10edaca80a
K10-K10 <keito.tottori@gmail.com>
2026-01-12 21:57:33 +0900

    format

--- src/layouts/BlogPost/BlogPost.tsx
+++ src/layouts/BlogPost/BlogPost.tsx
@@ -1,50 +1,56 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import './BlogPost.css';
-import { Post, posts } from '@pages/Blog/posts';
-import Talk from '@layouts/Talk/Talk';
-import Button from '@parts/Button';
-import NotFound from '@pages/404/404';
-import Icon from '@parts/Icon';
+import { useParams, useNavigate } from "react-router-dom";
+import "./BlogPost.css";
+import { Post, posts } from "@pages/Blog/posts";
+import Talk from "@layouts/Talk/Talk";
+import Button from "@parts/Button";
+import NotFound from "@pages/404/404";
+import Icon from "@parts/Icon";
 
 export default function BlogPost() {
-	const { headName } = useParams();
-	const navigate = useNavigate();
-	const post: Post | undefined = posts.find((p: Post) => p.headName === headName);
+  const { headName } = useParams();
+  const navigate = useNavigate();
+  const post: Post | undefined = posts.find(
+    (p: Post) => p.headName === headName
+  );
 
-	if (!post) return <NotFound />;
+  if (!post) return <NotFound />;
 
-	const handleNext = () => {
-		const nextPost = posts.find((item: Post) => item.id === post.id + 1);
-		if (nextPost) {
-			navigate(`/Blog/${nextPost.headName}`);
-		} else {
-			alert("This is the last post.");
-		}
-	};
-	const handleForward = () => {
-		const nextPost = posts.find((item: Post) => item.id === post.id - 1);
-		if (nextPost) {
-			navigate(`/Blog/${nextPost.headName}`);
-		} else {
-			alert("This is the first post.");
-		}
-	};
+  const handleNext = () => {
+    const nextPost = posts.find((item: Post) => item.id === post.id + 1);
+    if (nextPost) {
+      navigate(`/Blog/${nextPost.headName}`);
+    } else {
+      alert("This is the last post.");
+    }
+  };
+  const handleForward = () => {
+    const nextPost = posts.find((item: Post) => item.id === post.id - 1);
+    if (nextPost) {
+      navigate(`/Blog/${nextPost.headName}`);
+    } else {
+      alert("This is the first post.");
+    }
+  };
 
-	return (
-		<div className='blog-post'>
-			<Talk title={post.title}>
-				<small
-					style={{ textAlign: 'center', display: 'block', marginBottom: '30px' }}
-					className='data'
-				>
-					{post.date}
-				</small>
+  return (
+    <div className="blog-post">
+      <Talk title={post.title}>
+        <small
+          style={{
+            textAlign: "center",
+            display: "block",
+            marginBottom: "30px",
+          }}
+          className="data"
+        >
+          {post.date}
+        </small>
 
-				<pre style={{ lineHeight: '150%', whiteSpace: 'pre-wrap' }}>
-					{post.content}
-				</pre>
+        <pre style={{ lineHeight: "150%", whiteSpace: "pre-wrap" }}>
+          {post.content}
+        </pre>
 
-				{/*<div className="blog-buttons">
+        {/*<div className="blog-buttons">
 					<Button className="forwardBlog" onClick={handleForward} disabled={true}>
 						<Icon name="left" theme="re" />
 					</Button>
@@ -52,8 +58,7 @@ export default function BlogPost() {
 						<Icon name="right" theme="re" />
 					</Button>
 				</div>*/}
-			</Talk>
-		</div >
-	);
+      </Talk>
+    </div>
+  );
 }
-
