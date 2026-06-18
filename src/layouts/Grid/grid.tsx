35460e802d45b8bdf89790bc80d3e205594d3ffd
K10-K10 <keito.tottori@gmail.com>
2026-06-18 15:08:54 +0900

    feat: add new component grid

---
+++ src/layouts/Grid/grid.tsx
@@ -0,0 +1,15 @@
+import './gird.css';
+
+export default function Grid({ title, description, imageUrl, link }: { title: string, description: string, imageUrl?: string, link?: string }) {
+  return (
+    <div className="grid-item" onClick={() => link && window.open(link, '_blank')}>
+      <div className="grid-content">
+        {imageUrl && <img src={imageUrl} alt={title} className="grid-image" />}
+        <div className="grid-text">
+          <h2 className="grid-title">{title}</h2>
+          <p className="grid-description">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
