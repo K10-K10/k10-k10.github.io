0ddef03004fcb5e7f7e48402b2d0690553c70f6b
K10-K10 <keito.tottori@gmail.com>
2026-06-18 15:20:04 +0000

    feat: add tui docs home

---
+++ src/pages/Docs/TuiLib/tuilib_home.tsx
@@ -0,0 +1,45 @@
+import Talk from "@layouts/Talk/Talk";
+import Head from "@layouts/Head/Head";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import Code from '@parts/Code';
+
+import readme from '@contents/tuiLib/README.md?raw';
+
+export default function TuiDocs() {
+  return (
+    <div>
+      <Head
+        title={"K10-K10 - TUI Docs"}
+        linkTitle="tui"
+        description={
+          "K10-K10 Documentation - TUI library in C++ documentation."
+        }
+        pageUrl="https://K10-K10.github.io/Docs/TuiLib"
+      />
+      <Talk title="">
+        <Markdown 
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              const language = match ? match[1] : '';
+
+              return !inline ? (
+                <Code>
+                  {String(children).replace(/\n$/, '')}
+                </Code>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            }
+          }}
+        >
+          {readme}
+        </Markdown>
+      </Talk>
+    </div>
+  );
+}
