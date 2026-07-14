23409bd7fcf7f674fedd953d85dd5906eb56c350
K10-K10 <keito.tottori@gmail.com>
2026-07-14 14:19:42 +0000

    feat: add ssg data

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
+
   assetsInclude: ["**/*.md"],
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+
+  ssgOptions: {
+    script: "async",
+    formatting: "minify",
+    includedRoutes() {
+      return [
+        "/",
+        "/about",
+        "/blog",
+        "/docs",
+        "/docs/tuilib",
+        "/tools",
+        "/tools/rgb",
+        "/tools/github",
+        "/tools/ants",
+        "/404"
+      ];
+    }
+  },
+
   resolve: {
     tsconfigPaths: true,
   },
